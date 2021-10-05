1680326c1b80fd32cae68bdc11371c20fc6f56b1
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-10-05 13:30:48 +0200

    creating package registation with controller and servcoe for registering new user

---
+++ src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/registration/RegistrationController.java
@@ -0,0 +1,19 @@
+package com.github.clanhouse.tinderforprojects.tinderforprojects.registration;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest registrationRequest) {
+        return registrationService.register(registrationRequest);
+    }
+}
+}
