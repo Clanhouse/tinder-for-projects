d1b74a11ebc28e1064bec81ea8c7700f37487910
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-10-05 12:09:43 +0200

    removing not needed packgs

--- src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.clanhouse.tinderforprojects.tinderforprojects.repository;
-
-import com.github.clanhouse.tinderforprojects.tinderforprojects.entities.Company;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends CrudRepository<Company, Integer> {
-}
--- src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/repository/DeveloperRapository.java
+++
@@ -1,9 +0,0 @@
-package com.github.clanhouse.tinderforprojects.tinderforprojects.repository;
-
-import com.github.clanhouse.tinderforprojects.tinderforprojects.entities.Developer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeveloperRapository extends CrudRepository<Developer, Integer> {
-}
