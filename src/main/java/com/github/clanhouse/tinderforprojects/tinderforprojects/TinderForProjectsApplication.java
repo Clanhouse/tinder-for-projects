db8e0979b889c9b92164cd02d5ce8293bd365e77
Jakub Brózda <brozdajakub@gmail.com>
2021-10-12 20:55:49 +0200

    test commit

--- src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/TinderForProjectsApplication.java
+++ src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/TinderForProjectsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class TinderForProjectsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TinderForProjectsApplication.class, args);
     }
