b1b60064b5ef0768f9f0d3516b97cdf51a39a697
tomaszciula <tomciuns@gmail.com>
2021-10-19 17:00:01 +0200

    phone and email from npm added

--- ui/src/Components/UserProfile/UserProfile.js
+++ ui/src/Components/UserProfile/UserProfile.js
@@ -7,6 +7,9 @@ import { useHistory } from 'react-router-dom'
 import { profile } from './data'
 import AccountSettings from './AccountSettings/AccountSettings'
 import Settings from './Settings/Settings'
+import Email from 'react-email-autocomplete'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 function UserProfile() {
   const history = useHistory()
@@ -16,6 +19,7 @@ function UserProfile() {
   const [isPhone, setIsPhone] = useState(false)
   const [isLogout, setIsLogout] = useState(false)
   const [isDelete, setIsDelete] = useState(false)
+  const [actualPhone, setActualPhone] = useState()
 
   return (
     <div className="profile">
@@ -37,8 +41,7 @@ function UserProfile() {
         >
           <div className="nameModal">
             <span>Enter your name</span>
-            <input value={profile.name}></input>
-            <Button />
+            <input type="text" value={profile.name}></input>
           </div>
         </Modal>
         <Modal
@@ -49,9 +52,18 @@ function UserProfile() {
         >
           <div className="nameModal">
             <span>Enter your email</span>
-            <input value={profile.email}></input>
-            <Button />
-          </div>        </Modal>
+            <Email className="email--control" placeholder="Enter your email" />
+            <Button
+              children={undefined}
+              size={undefined}
+              ghost={undefined}
+              mobile={undefined}
+              fullWidth={undefined}
+              style={undefined}
+              onClick={undefined}
+            />
+          </div>
+        </Modal>
         <Modal
           open={isPhone}
           onClose={() => setIsPhone(false)}
@@ -60,9 +72,22 @@ function UserProfile() {
         >
           <div className="nameModal">
             <span>Enter your phone</span>
-            <input value={profile.phone}></input>
-            <Button />
-          </div>        </Modal>
+            <PhoneInput
+              placeholder="Enter phone number"
+              value={actualPhone}
+              onChange={() => setActualPhone}
+            />
+            <Button
+              children={undefined}
+              size={undefined}
+              ghost={undefined}
+              mobile={undefined}
+              fullWidth={undefined}
+              style={undefined}
+              onClick={undefined}
+            />
+          </div>{' '}
+        </Modal>
       </div>
 
       <div className="profile--settings">
@@ -95,7 +120,12 @@ function UserProfile() {
         </Button>
       </div>
 
-      <Modal open={isLogout} onClose={() => setIsLogout(false)} center showCloseIcon={false}>
+      <Modal
+        open={isLogout}
+        onClose={() => setIsLogout(false)}
+        center
+        showCloseIcon={false}
+      >
         <div className="profile__modal">
           <span>Do you really want to log out?</span>
           <div className="profile__modal--buttons">
@@ -105,7 +135,12 @@ function UserProfile() {
         </div>
       </Modal>
 
-      <Modal open={isDelete} onClose={() => setIsDelete(false)} center showCloseIcon={false}>
+      <Modal
+        open={isDelete}
+        onClose={() => setIsDelete(false)}
+        center
+        showCloseIcon={false}
+      >
         <div className="profile__modal">
           <span>Are you sure you want to delete your account?</span>
           <div className="profile__modal--buttons">
