1365df16894b2184a4c7a3109988108f49a9a96a
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-10-20 19:56:48 +0200

    pckg applicationUser -> extending ApplicationUserRepository with JpaRepository for DB and add @Service in pckg refistration-> RegistrationService

--- src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/registration/RegistrationService.java
+++ src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.github.clanhouse.tinderforprojects.tinderforprojects.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest registrationRequest) {
         return "ok";
