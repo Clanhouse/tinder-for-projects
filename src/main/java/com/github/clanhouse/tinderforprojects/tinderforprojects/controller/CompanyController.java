8e4cdbf458daa282639e539ab6bce9bc22ecd1c6
Jakub Brózda <brozdajakub@gmail.com>
2022-02-08 20:10:43 +0100

    fix

--- src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/controller/CompanyController.java
+++ src/main/java/com/github/clanhouse/tinderforprojects/tinderforprojects/controller/CompanyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/companys")
+@RequestMapping("/companies")
 public class CompanyController {
 
     private final CompanyService companyService;
