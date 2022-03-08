3fada10229a6ef2ceec7711d14cac612259a750d
Jakub Brózda <brozdajakub@gmail.com>
2022-03-08 19:49:55 +0100

    Initial commit

---
+++ src/main/java/com/tinderforprojects/tinder/TinderForProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.tinderforprojects.tinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderForProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinderForProjectsApplication.class, args);
+    }
+
+}
