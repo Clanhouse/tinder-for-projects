47c4a6cb83d97c77ec16fa3f97d746bec6047fa3
norsu296 <brozdajakub@gmail.com>
2022-03-19 17:59:33 +0100

    logging

--- src/main/java/com/tinderforprojects/tinder/model/benefit/BenefitServiceImpl.java
+++ src/main/java/com/tinderforprojects/tinder/model/benefit/BenefitServiceImpl.java
@@ -4,29 +4,37 @@ import com.tinderforprojects.tinder.exception.ErrorMessage;
 import com.tinderforprojects.tinder.exception.badRequest.BadRequestException;
 import com.tinderforprojects.tinder.exception.notFound.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BenefitServiceImpl implements BenefitService {
 
     private final BenefitRepository benefitRepository;
 
     @Override
     public List<Benefit> findAll() {
+        log.info("Downloading all benfits");
         return benefitRepository.findAll();
     }
 
     @Override
     public Benefit findById(Long id) {
+        log.info(String.format("Downloading benefit id: %d", id));
         return benefitRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
+                .orElseThrow(() -> {
+            log.error(String.format("Benefit id: %d does not exists", id));
+            return new NotFoundException(ErrorMessage.NOT_FOUND);
+        });
     }
 
     @Override
     public Benefit create(String name) {
+        log.info(String.format("Creating benefit: %s", name));
         validateName(name);
         return benefitRepository.save(
                 Benefit.builder()
@@ -37,15 +45,22 @@ public class BenefitServiceImpl implements BenefitService {
 
     @Override
     public Benefit update(Long id, String name) {
+        log.info(String.format("Updating benefit: %s , id: %d", name, id));
         validateName(name);
         return benefitRepository.findById(id)
                 .map(benefitFromDb -> {
                     benefitFromDb.setName(name);
                     return benefitRepository.save(benefitFromDb);
-                }).orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND));
+                }).orElseThrow(() -> {
+                    log.error(String.format("Benefit id: %d does not exists", id));
+                    return new NotFoundException(ErrorMessage.NOT_FOUND);
+                });
     }
 
     private void validateName(String name) {
-        if(name.length() < 3) throw new BadRequestException(ErrorMessage.BAD_REQUEST);
+        if(name.length() < 3) {
+            log.error(String.format("Invalid benefit name %s", name));
+            throw new BadRequestException(ErrorMessage.BAD_REQUEST);
+        }
     }
 }
