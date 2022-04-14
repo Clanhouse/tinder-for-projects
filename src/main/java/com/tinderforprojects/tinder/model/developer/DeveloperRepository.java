6f0a93082fe70ab0e69c340b8d8e43c3d14faf08
Jakub Brózda <brozdajakub@gmail.com>
2022-04-14 20:26:11 +0200

    fix heroku problems

--- src/main/java/com/tinderforprojects/tinder/model/developer/DeveloperRepository.java
+++ src/main/java/com/tinderforprojects/tinder/model/developer/DeveloperRepository.java
@@ -2,6 +2,7 @@ package com.tinderforprojects.tinder.model.developer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,6 +12,6 @@ public interface DeveloperRepository extends JpaRepository<Developer, Long> {
             "    left join table_to_matches ttm on developers.id = ttm.developer_id\n" +
             "where ttm.developer_id is null\n" +
             "   or ttm.project_id <> :projectId")
-    List<Developer> getRandomDevelopers(Long projectId);
+    List<Developer> getRandomDevelopers(@Param("projectId") Long projectId);
 
 }
