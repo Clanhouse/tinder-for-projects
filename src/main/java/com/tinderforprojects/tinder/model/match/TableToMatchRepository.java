883393b817e25cf36e6767ac22fc1bb8b2567f9f
Jakub Brózda <brozdajakub@gmail.com>
2022-04-17 12:13:48 +0200

    fix get liked

--- src/main/java/com/tinderforprojects/tinder/model/match/TableToMatchRepository.java
+++ src/main/java/com/tinderforprojects/tinder/model/match/TableToMatchRepository.java
@@ -4,6 +4,7 @@ import com.tinderforprojects.tinder.model.developer.Developer;
 import com.tinderforprojects.tinder.model.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +17,10 @@ public interface TableToMatchRepository extends JpaRepository<TableToMatch, Long
 
 
     @Query("SELECT p FROM TableToMatch t JOIN t.project p WHERE t.developer.id = :id AND t.isMatch = true")
-    List<Project> getAllLikedProjectsByDevId(Long id);
+    List<Project> getAllLikedProjectsByDevId(@Param("id") Long id);
 
     @Query("SELECT d FROM TableToMatch t JOIN t.developer d WHERE t.project.id = :id AND t.isMatch = true")
-    List<Developer> getAllLikedDevsByProjectId(Long id);
+    List<Developer> getAllLikedDevsByProjectId(@Param("id") Long id);
 
     Optional<Boolean> findMatchByProjectId(Long projectId);
     
